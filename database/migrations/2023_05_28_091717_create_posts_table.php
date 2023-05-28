4e41aeaf50f1dfa308a860e0bd577992e4c564da
Sufyan Muzaffar <sufyanmuzaffar67@gmail.com>
2023-05-28 15:10:46 +0500

    Post and Category modal and migration created

---
+++ database/migrations/2023_05_28_091717_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('posts')){
+            Schema::create('posts', function (Blueprint $table) {
+                $table->id();
+                $table->integer('category_id');
+                $table->string('thumbnail')->nullable();
+                $table->string('description');
+                $table->integer('status')->default(0);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
